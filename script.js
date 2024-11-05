333dcaa4fc5e0bb410d83505bb4fbdec0833a206
Alejandro Anibal Andrade <alejandro21112@hotmail.com>
2024-11-04 22:48:23 -0500

    Idea changed
    La idea se cambió de miscelania a panaderia.

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 document.getElementById("product-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita que la página se recargue al enviar el formulario
+    event.preventDefault(); 
 
     // Captura los valores de los campos
     const productName = document.getElementById("product-name").value;
+    const productMarca = document.getElementById("product-marca").value;
+    const productDate = document.getElementById("product-date").value;
     const productQuantity = document.getElementById("product-quantity").value;
 
     if (productName && productQuantity) {
         // Crea un nuevo elemento para el producto
         const productItem = document.createElement("div");
-        productItem.textContent = `${productName} - Cantidad: ${productQuantity}`;
+        productItem.textContent = `${productName} - Marca: ${productMarca} - Cantidad: ${productQuantity} - Fecha Vencimiento ${productDate}`;
         
         // Agrega el producto a la lista
         document.getElementById("product-list").appendChild(productItem);
